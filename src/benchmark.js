4f001ade52431c429cc3fec883dcd046ea4aa85d
alexander.beletsky <alexander.beletsky@gmail.com>
2012-10-28 08:08:11 +0200

    refactoring

--- src/benchmark.js
+++ src/benchmark.js
@@ -8,88 +8,92 @@ module.exports = (function () {
             _.bindAll(this);
         },
 
-        createStrategySync: function (options) {
-            if (options.repeat && options.average) {
-                return average;
-            }
-
-            if (options.repeat) {
-                return repeat;
-            }
-
-            return simple;
-            
-            function repeat(actionName, f) {
-                var start = new Date().getTime();
-                var message = actionName + ' (repeated ' + options.repeat + ' times) took: ';
-                var seconds = ' msec.';
-
-                for (var i = 0; i < options.repeat; i++) {
-                    f();
+        sync: {
+            createStrategy: function (options) {
+                if (options.repeat && options.average) {
+                    return average;
                 }
 
-                var finish = new Date().getTime();
-                var duration = finish - start;
-
-                console.log (message + duration + seconds);
-            }
-
-            function average(actionName, f) {
-                var start = new Date().getTime();
-                var message = actionName + ' (repeated ' + options.repeat + ' times) took: ';
-                var seconds = ' msec. (in average)';
-
-                for (var i = 0; i < options.repeat; i++) {
-                    f();
+                if (options.repeat) {
+                    return repeat;
                 }
 
-                var finish = new Date().getTime();
-                var duration = (finish - start) / options.repeat;
-
-                console.log (message + duration + seconds);
-            }
-
-            function simple(actionName, f) {
-                var start = new Date().getTime();
-                var message = actionName + ' took: ';
-                var seconds = ' msec.';
-
-                f();
-
-                var finish = new Date().getTime();
-                var duration = finish - start;
-
-                console.log (message + duration + seconds);
-            }
-        },
-
-        syncWrap: function syncWrap (actionName, options, func) {
-            var me = this;
-
-            return _.wrap(func, function(f, next) {
-                var strategy = me.createStrategySync(options);
+                return simple;
                 
-                strategy(actionName, f);
+                function repeat(actionName, f) {
+                    var start = new Date().getTime();
+                    var message = actionName + ' (repeated ' + options.repeat + ' times) took: ';
+                    var seconds = ' msec.';
 
-                next();
-            });
-        },
+                    for (var i = 0; i < options.repeat; i++) {
+                        f();
+                    }
 
-        asyncWrap: function asyncWrap(actionName, options, func) {
-            return _.wrap(func, function (f, next) {
-                var start = new Date().getTime();
-
-                var callback = function () {
                     var finish = new Date().getTime();
                     var duration = finish - start;
 
-                    console.log (actionName + ' took: ' + duration + ' msec.');
+                    console.log (message + duration + seconds);
+                }
+
+                function average(actionName, f) {
+                    var start = new Date().getTime();
+                    var message = actionName + ' (repeated ' + options.repeat + ' times) took: ';
+                    var seconds = ' msec. (in average)';
+
+                    for (var i = 0; i < options.repeat; i++) {
+                        f();
+                    }
+
+                    var finish = new Date().getTime();
+                    var duration = (finish - start) / options.repeat;
+
+                    console.log (message + duration + seconds);
+                }
+
+                function simple(actionName, f) {
+                    var start = new Date().getTime();
+                    var message = actionName + ' took: ';
+                    var seconds = ' msec.';
+
+                    f();
+
+                    var finish = new Date().getTime();
+                    var duration = finish - start;
+
+                    console.log (message + duration + seconds);
+                }
+            },
+
+            wrap: function syncWrap (actionName, options, func) {
+                var me = this;
+
+                return _.wrap(func, function(f, next) {
+                    var strategy = me.createStrategySync(options);
+                    
+                    strategy(actionName, f);
 
                     next();
-                };
+                });
+            }
+        },
 
-                f(callback);
-            });
+        async: {
+            wrap: function asyncWrap(actionName, options, func) {
+                return _.wrap(func, function (f, next) {
+                    var start = new Date().getTime();
+
+                    var callback = function () {
+                        var finish = new Date().getTime();
+                        var duration = finish - start;
+
+                        console.log (actionName + ' took: ' + duration + ' msec.');
+
+                        next();
+                    };
+
+                    f(callback);
+                });
+            }
         },
 
         establishContext: function (title, callback) {
@@ -109,7 +113,7 @@ module.exports = (function () {
             }
 
             options = options || {};
-            var wrap = func.length === 0 ? this.syncWrap(actionName, options, func) : this.asyncWrap(actionName, options, func);
+            var wrap = func.length === 0 ? this.sync.wrap(actionName, options, func) : this.async.wrap(actionName, options, func);
 
             context.functions.push(wrap);
         }
